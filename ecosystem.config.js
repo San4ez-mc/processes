fdcb609121a79cdb761296998804b9f07066397a
unknown <olexandrmatsuk@gmail.com>
2026-04-22 22:46:23 +0300

    chore: stable baseline before scenario 3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  apps: [
-    {
-      name: 'business-process-agent',
-      script: 'src/bot.js',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '512M',
-      env: {
-        NODE_ENV: 'production'
-      },
-      error_file: 'logs/error.log',
-      out_file: 'logs/out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss'
-    }
-  ]
+    apps: [
+        {
+            name: 'business-process-agent',
+            script: 'src/bot.js',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '512M',
+            env: {
+                NODE_ENV: 'production'
+            },
+            error_file: 'logs/error.log',
+            out_file: 'logs/out.log',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss'
+        }
+    ]
 }
